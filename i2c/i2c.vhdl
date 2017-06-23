5c6d38de8c0785f7d1babe4b4fd5ad3d88e74746
Ivin <ivinstephen001@gmail.com>
2017-06-23 19:09:59 +0530

    add ack

--- i2c/i2c.vhdl
+++ i2c/i2c.vhdl
@@ -6,31 +6,115 @@ use ieee.math_real.all;                 -- for the ceiling and log constant calc
 
 entity i2c is
 	port (
-		clock : IN std_logic;
-		reset : IN std_logic;
-		en   	: IN std_logic;
-		r_w   : IN std_logic;
-		reg	  : INOUT std_logic_vector(7 downto 0);
+		clock 	: IN std_logic;
+		reset 	: IN std_logic;
+		en    	: IN std_logic;
+		r_w   	: IN std_logic;
+		reg	  	: IN std_logic_vector(7 downto 0);
 
-		scl   : OUT std_logic;
-		sda   : INOUT std_logic
-	);
+		sda_ext : INOUT std_logic;
+		scl   	: OUT std_logic
+		);
 end i2c;
 
 architecture i2c_arc of i2c is
 
+	TYPE machine IS(ready, start, command1, command2, command3, command4, command_end, 
+								  ack, ack2, ack3, ack4,
+								  msg1, msg2, msg3, msg4, msg_end, stop); --needed states
+    signal state : machine;
+    signal sda : std_logic;
+	signal bit_cnt : INTEGER range 0 to 7 := 7;
+
 begin
 
-	reg <= x"aa";
+	sda_ext <= 'Z' WHEN (sda = '1') ELSE '0';
+	
 	main : process (reset, clock)
 	begin
-	  if (reset = '1') then
-	    sda <= '1';
-	    scl <= '1';
-	  elsif (rising_edge(clock)) then
- 			sda <= not sda;
-	    scl <= not scl;
-	  end if;
+		if (reset = '1') then
+			sda <= '1';
+			scl <= '1';
+		elsif (rising_edge(clock) and (en = '1')) then
+			case(state) is
+				when ready =>      
+					sda <= '1';
+					scl <= '1';
+					state <= start;
 
+				when start =>     	
+					sda <= '0';
+					scl <= '1';
+					state <= command1;
+
+				when command1 =>		
+					scl <= '0';
+					state <= command2;
+				
+				when command2 =>	
+					sda <= reg(bit_cnt);
+					state <= command3;
+					
+				when command3 =>  
+					scl <= '1';
+					state <= command4;
+
+				when command4 =>
+					if (bit_cnt - 1) >= 0 then
+						bit_cnt <= bit_cnt - 1;
+						state <= command1;
+					else
+						bit_cnt <= 7;
+						state <= command_end;
+					end if;
+
+				when command_end =>
+					scl <= '0';
+					state <= ack;
+
+				when ack =>
+					sda <= sda_ext;
+					state <= ack2;
+
+				when ack2 =>
+					scl <= '1';
+					state <= ack3; 
+
+				when ack3 =>
+					state <= ack4; 
+
+				when ack4 =>
+					scl <= '0';
+					state <= msg1; 
+
+				when msg1 =>		
+					scl <= '0';
+					state <= msg2;
+				
+				when msg2 =>	
+					sda <= reg(bit_cnt);
+					state <= msg3;
+					
+				when msg3 =>  
+					scl <= '1';
+					state <= msg4;
+
+				when msg4 =>
+					if (bit_cnt - 1) >= 0 then
+						bit_cnt <= bit_cnt - 1;
+						state <= msg1;
+					else
+						bit_cnt <= 7;
+						state <= msg_end;
+					end if;
+
+				when msg_end =>
+					scl <= '0';
+					state <= stop;
+
+				when others =>
+					null;
+			end case;
+		end if;
 	end process main;
 end i2c_arc;
--- i2c/i2c_tb.vhdl
+++ i2c/i2c_tb.vhdl
@@ -11,33 +11,54 @@ end testbench;
 architecture tb of testbench is
 	component i2c is
 	port (
-		clock : IN std_logic;
-		reset : IN std_logic;
+		clock 	: IN std_logic;
+		reset 	: IN std_logic;
 		en   	: IN std_logic;
-		r_w   : IN std_logic;
-		reg	  : INOUT std_logic_vector(7 downto 0);
+		r_w   	: IN std_logic;
+		reg	  	: IN std_logic_vector(7 downto 0);
 
-		scl   : OUT std_logic;
-		sda   : INOUT std_logic
+		sda_ext : INOUT std_logic;
+		scl   	: OUT std_logic
 	);
 	end component;
 
 	signal tb_clock, tb_reset, tb_en, tb_r_w : std_logic;
-	signal tb_scl, tb_sda : std_logic;
-	signal tb_reg	: std_logic_vector(7 downto 0);
+	signal tb_sda, tb_scl : std_logic;
+	signal tb_reg	: std_logic_vector(7 downto 0) := x"aa";
+	constant max_clock : integer := (50);
 
 begin
 
-	UUT: i2c port map (tb_clock, tb_reset, tb_en, tb_r_w, tb_reg, tb_scl, tb_sda);
+  UUT: i2c port map (tb_clock, tb_reset, tb_en, tb_r_w, tb_reg, tb_sda, tb_scl);
 
-	clk_gen: process
+
+  clk_gen: process
   begin
-    for i in 0 to 3 loop
-      tb_clock <= '0';
-      wait for 0.5 ns;
+
+  	tb_reset <= '1'; 
+	wait for 2 ns;
+	tb_reset <= '0';
+	wait for 1 ns;
+
+	tb_reg <= x"aa";
+
+	tb_en <= '1';
+
+
+    for i in 0 to max_clock loop
+
+      if (i = 36) then
+      	tb_sda <= '0';
+      else
+      	tb_sda <= 'H';
+      end if;
+
       tb_clock <= '1';
       wait for 0.5 ns;
-		end loop;
+      tb_clock <= '0';
+      wait for 0.5 ns;
+
+	end loop;
     wait;
   end process clk_gen;
 
